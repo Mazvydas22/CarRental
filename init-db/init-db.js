803ca8aad46864102b99104314e6770da97527c4
Mazvydas22 <113maz@gmail.com>
2021-04-14 11:32:22 +0300

    fix3

--- init-db/init-db.js
+++ init-db/init-db.js
@@ -1,4 +1,4 @@
-db.Cars.insertMany([
+db.mongo.insertMany([
     {"Brand": "Toyota", "model": "Corolla", "year":"1999", "price": "50" },
     {"Brand": "Nissan", "model": "Almera", "year":"1999", "price": "50" },
     {"Brand": "BMW", "model": "E60", "year":"1999", "price": "50" },
